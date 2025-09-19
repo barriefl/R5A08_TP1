ab60d91d51cfbbbffcf7cf906a3b60d10e2569b4
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-09-19 15:38:50 +0200

    Ajout services dans BlazorApp.

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,3 +1,5 @@
+using BlazorApp.Models;
+using BlazorApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,7 +13,7 @@ namespace BlazorApp
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IService<Product>, WebService>(_ => new WebService());
 
             await builder.Build().RunAsync();
         }
--- BlazorApp/Services/WebService.cs
+++ BlazorApp/Services/WebService.cs
@@ -1,6 +1,46 @@
-﻿namespace BlazorApp.Services
+﻿using BlazorApp.Models;
+using System.Net.Http.Json;
+
+namespace BlazorApp.Services
 {
-    public class WebService
+    public class WebService : IService<Product>
     {
+        private readonly HttpClient httpClient = new()
+        {
+            BaseAddress = new Uri("http://localhost:5011/api/")
+        };
+
+        public async Task AddAsync(Product product)
+        {
+            await httpClient.PostAsJsonAsync<Product>("products", product);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await httpClient.DeleteAsync($"product/{id}");
+        }
+
+        public async Task<IEnumerable<Product>?> GetAllAsync()
+        {
+            return await httpClient.GetFromJsonAsync<IEnumerable<Product>?>("products");
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await httpClient.GetFromJsonAsync<Product?>($"product/{id}");
+        }
+
+        public async Task<Product?> GetByNameAsync(string name)
+        {
+            var response = await httpClient.PostAsJsonAsync("products/search", name);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<Product>();
+        }
+
+        public async Task UpdateAsync(Product updatedEntity)
+        {
+            await httpClient.PutAsJsonAsync<Product>($"products/{updatedEntity.IdProduit}", updatedEntity);
+        }
     }
 }
