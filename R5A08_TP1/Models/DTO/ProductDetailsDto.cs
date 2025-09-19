5223086a398ca8cfab8141782c4d7e02f34abb09
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-09-19 15:38:36 +0200

    Ajout DTO dans l'API.

---
+++ R5A08_TP1/Models/DTO/ProductDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace R5A08_TP1.Models.DTO
+{
+    public class ProductDetailsDto
+    {
+        public int Id { get; set; }
+        public string? Nom { get; set; }
+        public string? Type { get; set; }
+        public string? Marque { get; set; }
+        public string? Description { get; set; }
+        public string? Nomphoto { get; set; }
+        public string? Uriphoto { get; set; }
+        public int? Stock { get; set; }
+        public bool EnReappro { get; set; }
+    }
+}
---
+++ R5A08_TP1/Models/DTO/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace R5A08_TP1.Models.DTO
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string? Nom { get; set; }
+        public string? Type { get; set; }
+        public string? Marque { get; set; }
+    }
+}
