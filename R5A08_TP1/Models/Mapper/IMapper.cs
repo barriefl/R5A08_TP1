1f4922d79f7f9bda5e0cf6d36709191773b32b69
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-09-19 16:23:32 +0200

    Création Mapper.

---
+++ R5A08_TP1/Models/Mapper/IMapper.cs
@@ -0,0 +1,18 @@
+﻿namespace R5A08_TP1.Models.Mapper
+{
+    public interface IMapper<Entity, DTO>
+    {
+        Entity? FromDTO(DTO dto);
+        DTO? FromEntity(Entity entity);
+
+        IEnumerable<DTO> ToDTO(IEnumerable<Entity> entities)
+        {
+            return entities.Select(e => FromEntity(e));
+        }
+
+        IEnumerable<DTO> QuelquechoseDTO(IEnumerable<Entity> entities)
+        {
+            return entities.Select(e => FromEntity(e));
+        }
+    }
+}
---
+++ R5A08_TP1/Models/Mapper/ProductMapper.cs
@@ -0,0 +1,30 @@
+﻿using R5A08_TP1.Models.DTO;
+using R5A08_TP1.Models.EntityFramework;
+
+namespace R5A08_TP1.Models.Mapper
+{
+    public class ProductMapper : IMapper<Produit, ProductDto>
+    {
+        public Produit? FromDTO(ProductDto dto)
+        {
+            return new Produit
+            {
+                IdProduit = dto.Id,
+                NomProduit = dto.Nom,
+                TypeProduitNavigation = new TypeProduit { NomTypeProduit = dto.Type },
+                MarqueNavigation = new Marque { NomMarque = dto.Marque }
+                };
+        }
+
+        public ProductDto? FromEntity(Produit entity)
+        {
+            return new ProductDto
+            {
+                Id = entity.IdProduit,
+                Nom = entity.NomProduit,
+                Type = entity.TypeProduitNavigation.NomTypeProduit,
+                Marque = entity.MarqueNavigation.NomMarque
+            };
+        }
+    }
+}
