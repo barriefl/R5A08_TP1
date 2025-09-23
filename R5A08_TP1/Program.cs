5c9f7a7aa5ead7bafb20e4aa484275d5da82794b
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-09-23 15:29:25 +0200

    Injection de dépendance.

--- R5A08_TP1/Program.cs
+++ R5A08_TP1/Program.cs
@@ -6,10 +6,14 @@ using R5A08_TP1.Models.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IWriteDataRepository<Product>, GetDataManager>();
+builder.Services.AddScoped<IProductRepository, ProductManager>();
+builder.Services.AddScoped<IBrandRepository, BrandManager>();
+builder.Services.AddScoped<IProductTypeRepository, ProductTypeManager>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("ProduitsDbContext")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AppDbContext")));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); // AutoMapper configuration
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
